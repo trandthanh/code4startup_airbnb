b657dd17698902dd28ccb908d6fa88ff9b449e89
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-11-10 14:39:33 +0100

    installed toastr notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,15 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
   end
+
+  def toastr_flash_class(type)
+    case type
+    when "alert"
+      "toastr.error"
+    when "notice"
+      "toastr.success"
+    else
+      "toastr.info"
+    end
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+// import toastr from 'toastr';
+
+// toastr.options = {
+//     "closeButton": true
+// };
+
+// global.toastr = toastr;
