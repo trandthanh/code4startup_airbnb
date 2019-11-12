64db9ef10ed6c278114467c86bd89533a87ac9cb
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-11-12 17:04:39 +0100

    changed delivery host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://desolate-gorge-61612.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "desolate-gorge-61612.herokuapp.com" }
+  config.action_mailer.raise_delivery_errors = true
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
